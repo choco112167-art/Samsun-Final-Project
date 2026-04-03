f7c4fb0f05da2713b055469118d8a0b246107279
정수민(생성AI_7회차) <jsmin2080@gmail.com>
2026-04-03 17:28:22 +0900

    프론트엔드 04/03

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { TDSMobileAITProvider } from '@toss/tds-mobile-ait'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/global.css';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TDSMobileAITProvider>
-      <App />
-    </TDSMobileAITProvider>
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
